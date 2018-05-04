22511851bec7047843217d95975fa74bb7c5239b
Vetle W. Ingeberg <vetlewi@users.noreply.github.com>
2018-05-04 11:55:13 +0200

    Cosmetics...

--- XIAreader/source/system/include/TDRWordBuffer.h
+++ XIAreader/source/system/include/TDRWordBuffer.h
@@ -32,10 +32,9 @@ typedef struct {
     uint16_t adcdata;		//!< Data read out from the ADC.
     uint16_t cfddata;       //!< Fractional difference of before/after zero-crossing.
     char cfdfail;           //!< Flag to tell if the CFD was forced or not.
-    char finishcode;        //!< Pile-up flag
+    char finishcode;        //!< Pile-up flag.
     int64_t timestamp;		//!< Timestamp in [ns].
     double cfdcorr;         //!< Correction from the CFD.
-
 } word_t;
 
 
